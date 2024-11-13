0d754fdf7c3116d20acefdac52b555f5307e6b59
qiuxan <qiuxan@gmail.com>
2024-11-13 14:51:07 +1100

    EACT-2: improve code readablity

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,3 +1,5 @@
 export const ASC = "ASC";
 export const DESC = "DESC";
 export const NAME = 'name';
+export const BAND_NAME = 'bandName';
+export const RECORD_LABEL = 'recordLabel';
--- src/util/toolKits.ts
+++ src/util/toolKits.ts
@@ -1,10 +1,10 @@
 import { BandDto } from '../models/dto/BandDto';
 import { RecordDto } from '../models/dto/RecordDto';
 import { MusicFestival } from '../models/MusicFestival';
-import { ASC, DESC } from './constants';
+import { ASC, DESC,BAND_NAME,RECORD_LABEL } from './constants';
 
 interface ToolKit{
-    sortByPropertyName<T>(array: T[], property: keyof T, order: typeof ASC | typeof DESC): T[];
+    sortAnObjectArrayByPropertyName<T>(array: T[], property: keyof T, order: typeof ASC | typeof DESC): T[];
     mapMusicFestivalArrayToRecordDtoArray(musicFestivalGroup: MusicFestival[]): RecordDto[];
     sortStringArray(array: string[], order: typeof ASC | typeof DESC): string[];
 }
@@ -24,7 +24,7 @@ export const toolKit: ToolKit = {
             return 0;
         });
     },
-    sortByPropertyName<T>(array: T[], property: keyof T, order: typeof ASC | typeof DESC = ASC): T[] {
+    sortAnObjectArrayByPropertyName<T>(array: T[], property: keyof T, order: typeof ASC | typeof DESC = ASC): T[] {
         if (array.length === 0) {
             return array;
         }
@@ -63,14 +63,14 @@ export const toolKit: ToolKit = {
                 bandDtoGroupToStore.push({bandName, festivalNameGroup: bandFestivalGroupLabellMap.get(bandName) || []});
             }); 
             
-            bandDtoGroupToStore = this.sortByPropertyName(bandDtoGroupToStore, 'bandName', ASC);
+            bandDtoGroupToStore = this.sortAnObjectArrayByPropertyName(bandDtoGroupToStore, BAND_NAME, ASC);
 
             recordDtoToStore = {recordLabel, bandDtoGroup: bandDtoGroupToStore};
             recordDtoGroup.push(recordDtoToStore);
             
         });
 
-        recordDtoGroup = this.sortByPropertyName(recordDtoGroup, 'recordLabel', ASC);
+        recordDtoGroup = this.sortAnObjectArrayByPropertyName(recordDtoGroup, RECORD_LABEL, ASC);
 
         return recordDtoGroup;
     }
